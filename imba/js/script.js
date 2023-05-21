13c8f6da40d3d0fd34de15f8796a20268efea104
zaeraz7 <107050697+zaeraz7@users.noreply.github.com>
2023-05-21 13:40:08 +0300

    Add files via upload

---
+++ imba/js/script.js
@@ -0,0 +1,14 @@
+function close_advert() {
+	document.getElementsByClassName("advert")[0].style.display = "none";
+	document.querySelector('.hat').style.top = "0px";
+}
+
+
+
+function open_reg() {
+	document.querySelector('.bg_block').style.display = "flex";
+}
+
+function close_reg() {
+	document.querySelector('.bg_block').style.display = "none";
+}
